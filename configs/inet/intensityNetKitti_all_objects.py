6dd9919c63d980113b0547a440983955d16b5e90
a.y.naich@qmul.ac.uk <ammar.naich@gmail.com>
2022-07-16 23:20:15 +0100

    updating the config file

--- configs/inet/intensityNetKitti_all_objects.py
+++ configs/inet/intensityNetKitti_all_objects.py
@@ -1,8 +1,107 @@
+voxel_size = [0.05, 0.05, 0.1]
+point_cloud_range = [0, -40, -3, 70.4, 40, 1]
+
+model = dict(
+    type='VoxelNet',
+    voxel_layer=dict(
+        max_num_points=5,
+        point_cloud_range=[0, -40, -3, 70.4, 40, 1],
+        voxel_size=voxel_size,
+        max_voxels=(16000, 40000)),
+    voxel_encoder=dict(
+        type='IEVFE',      # HardVFE , IEVFE
+        in_channels=4,
+        feat_channels=[32, 128],
+        with_distance=False,
+        voxel_size=voxel_size,
+        with_cluster_center=True,
+        with_voxel_center=False,
+        point_cloud_range=point_cloud_range),
+    middle_encoder=dict(
+        type='SparseEncoder',
+        in_channels=138,
+        sparse_shape=[41, 1600, 1408],
+        order=('conv', 'norm', 'act')),
+    backbone=dict(
+        type='SECOND',
+        in_channels=256,
+        layer_nums=[5, 5],
+        layer_strides=[1, 2],
+        out_channels=[128, 256]),
+    neck=dict(
+        type='SECONDFPN',
+        in_channels=[128, 256],
+        upsample_strides=[1, 2],
+        out_channels=[256, 256]),
+    bbox_head=dict(
+        type='Anchor3DHead',
+        num_classes=3,
+        in_channels=512,
+        feat_channels=512,
+        use_direction_classifier=True,
+        anchor_generator=dict(
+            type='Anchor3DRangeGenerator',
+            ranges=[
+                [0, -40.0, -0.6, 70.4, 40.0, -0.6],
+                [0, -40.0, -0.6, 70.4, 40.0, -0.6],
+                [0, -40.0, -1.78, 70.4, 40.0, -1.78],
+            ],
+            sizes=[[0.8, 0.6, 1.73], [1.76, 0.6, 1.73], [3.9, 1.6, 1.56]],
+            rotations=[0, 1.57],
+            reshape_out=False),
+        diff_rad_by_sin=True,
+        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=2.0),
+        loss_dir=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.2)),
+    # model training and testing settings
+    train_cfg=dict(
+        assigner=[
+            dict(  # for Pedestrian
+                type='MaxIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.35,
+                neg_iou_thr=0.2,
+                min_pos_iou=0.2,
+                ignore_iof_thr=-1),
+            dict(  # for Cyclist
+                type='MaxIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.35,
+                neg_iou_thr=0.2,
+                min_pos_iou=0.2,
+                ignore_iof_thr=-1),
+            dict(  # for Car
+                type='MaxIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.6,
+                neg_iou_thr=0.45,
+                min_pos_iou=0.45,
+                ignore_iof_thr=-1),
+        ],
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        use_rotate_nms=True,
+        nms_across_levels=False,
+        nms_thr=0.01,
+        score_thr=0.1,
+        min_bbox_size=0,
+        nms_pre=100,
+        max_num=50))
+
+
 # dataset settings
 dataset_type = 'KittiDataset'
 data_root = '/workspace/data/kitti/'
 class_names = ['Pedestrian', 'Cyclist', 'Car']
-point_cloud_range = [0, -40, -3, 70.4, 40, 1]
 input_modality = dict(use_lidar=True, use_camera=False)
 db_sampler = dict(
     data_root=data_root,
@@ -96,7 +195,7 @@ eval_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=4,
+    samples_per_gpu=6,
     workers_per_gpu=4,
     train=dict(
         type='RepeatDataset',
@@ -139,125 +238,14 @@ data = dict(
 
 evaluation = dict(interval=1, pipeline=eval_pipeline)
 
-
-'''
-Model parameter settings
-'''
-# voxel_size = [0.2, 0.2, 0.4]
-# voxel_size = [0.05, 0.05, 0.2]
-voxel_size = [0.05, 0.05, 0.1]
-point_cloud_range=[0, -40, -3, 70.4, 40, 1]
-
-#  type='DynamicVFE',  # HardVFE , IEVFE' , HardSimpleVFE
-
-model = dict(
-    type='VoxelNet', # Type of the Detector, refer to mmdet3d.models.detectors 
-    voxel_layer=dict(
-        max_num_points=5,
-        point_cloud_range=[0, -40, -3, 70.4, 40, 1],
-        voxel_size=voxel_size,
-        max_voxels=(16000, 40000)),
-  
-    voxel_encoder=dict(
-        type='IEVFE',      # HardVFE , IEVFE
-        in_channels=4,
-        feat_channels=[32, 128],
-        with_distance=False,
-        voxel_size=voxel_size,
-        with_cluster_center=True,
-        with_voxel_center=False,
-        point_cloud_range=point_cloud_range),
-   
-    middle_encoder=dict(
-        type='SparseEncoder',
-        in_channels=138,
-        sparse_shape=[41, 1600, 1408],
-        order=('conv', 'norm', 'act')),
-    backbone=dict(
-        type='SECOND',
-        in_channels=256,
-        layer_nums=[5, 5],
-        layer_strides=[1, 2],
-        out_channels=[128, 256]),
-    neck=dict(
-        type='SECONDFPN',
-        in_channels=[128, 256],
-        upsample_strides=[1, 2],
-        out_channels=[256, 256]),
-     bbox_head=dict(
-        type='Anchor3DHead',
-        num_classes=3,
-        in_channels=512,
-        feat_channels=512,
-        use_direction_classifier=True,
-        anchor_generator=dict(
-            type='Anchor3DRangeGenerator',
-            ranges=[
-                [0, -40.0, -0.6, 70.4, 40.0, -0.6],
-                [0, -40.0, -0.6, 70.4, 40.0, -0.6],
-                [0, -40.0, -1.78, 70.4, 40.0, -1.78],
-            ],
-            sizes=[[0.8, 0.6, 1.73], [1.76, 0.6, 1.73], [3.9, 1.6, 1.56]],
-            rotations=[0, 1.57],
-            reshape_out=False),
-        diff_rad_by_sin=True,
-        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
-        loss_cls=dict(
-            type='FocalLoss',
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=2.0),
-        loss_dir=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.2)),
-    # model training and testing settings
-    train_cfg=dict(
-        assigner=[
-            dict(  # for Pedestrian
-                type='MaxIoUAssigner',
-                iou_calculator=dict(type='BboxOverlapsNearest3D'),
-                pos_iou_thr=0.35,
-                neg_iou_thr=0.2,
-                min_pos_iou=0.2,
-                ignore_iof_thr=-1),
-            dict(  # for Cyclist
-                type='MaxIoUAssigner',
-                iou_calculator=dict(type='BboxOverlapsNearest3D'),
-                pos_iou_thr=0.35,
-                neg_iou_thr=0.2,
-                min_pos_iou=0.2,
-                ignore_iof_thr=-1),
-            dict(  # for Car
-                type='MaxIoUAssigner',
-                iou_calculator=dict(type='BboxOverlapsNearest3D'),
-                pos_iou_thr=0.6,
-                neg_iou_thr=0.45,
-                min_pos_iou=0.45,
-                ignore_iof_thr=-1),
-        ],
-        allowed_border=0,
-        pos_weight=-1,
-        debug=False),
-    test_cfg=dict(
-        use_rotate_nms=True,
-        nms_across_levels=False,
-        nms_thr=0.01,
-        score_thr=0.1,
-        min_bbox_size=0,
-        nms_pre=100,
-        max_num=50))
-
-
-
 # custom_hooks = [ dict(type='TensorboardImageLoggerHook') ]
-# yapf:enable
+# Training settings
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = '/workspace/working_dir'
+work_dir = '/workspace/mmdetection3d/working_dir/intensityAware'
 load_from = None
 resume_from = None
-workflow = [('train', 1)]   # , ('val', 1)
+workflow = [('train', 1), ('val', 1)]
 
 # disable opencv multithreading to avoid system being overloaded
 opencv_num_threads = 0
@@ -265,14 +253,11 @@ opencv_num_threads = 0
 mp_start_method = 'fork'
 
 
-# fp16 settings
+# Mixed precision set to dynamic
 # fp16 =dict(loss_scale=512.)
 fp16 = dict(loss_scale='dynamic')
 
-'''
-Log settings
-'''
-checkpoint_config = dict(interval=1,max_keep_ckpts=5,save_last=True)
+checkpoint_config = dict(interval=1, max_keep_ckpts=5, save_last=True)
 log_config = dict(
     interval=50,
     hooks=[dict(type='TextLoggerHook'),
@@ -286,7 +271,6 @@ log_config = dict(
 #                         schedule = dict( wait=1,warmup=1,active=2),
 #                         on_trace_ready=dict(type='tb_trace', dir_name= work_dir))
 #                         # with_stack =True,
-
 
 
 '''
--- configs/second/hv_second_secfpn_fp16_6x8_80e_kitti-3d-car.py
+++ configs/second/hv_second_secfpn_fp16_6x8_80e_kitti-3d-car.py
@@ -1,4 +1,5 @@
 _base_ = './hv_second_secfpn_6x8_80e_kitti-3d-car.py'
+work_dir = "/workspace/mmdetection3d/working_dir/second_car"
 # fp16 settings
 # fp16 = dict(loss_scale=512.)
 fp16 = dict(loss_scale='dynamic')
