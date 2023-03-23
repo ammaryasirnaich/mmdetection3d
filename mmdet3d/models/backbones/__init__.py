28594447821793e8601b4191741a2aa161b29f66
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-03-23 16:05:31 +0000

    Set Point aggregation using with out FPS via voxel mean coordinates

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -9,9 +9,11 @@ from .pointnet2_sa_msg import PointNet2SAMSG
 from .pointnet2_sa_ssg import PointNet2SASSG
 from .second import SECOND
 from .VisionTransformer import ConViT3DDecoder,PointEmbed
+from .pointnet2_sa_ssg_sampleless import PointNet2SASSG_SL
 
 __all__ = [
     'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'NoStemRegNet',
     'SECOND', 'DGCNNBackbone', 'PointNet2SASSG', 'PointNet2SAMSG',
-    'MultiBackbone', 'DLANet', 'MinkResNet', 'ConViT3DDecoder', "PointEmbed"
+    'MultiBackbone', 'DLANet', 'MinkResNet', 'ConViT3DDecoder', "PointEmbed" ,
+    "PointNet2SASSG_SL"
 ]
