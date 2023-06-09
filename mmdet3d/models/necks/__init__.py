5cf214db25801978f2f59763e47596f5736b412a
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-06-09 18:20:05 +0000

    added the convit3d_neck in the neck module

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -5,8 +5,9 @@ from .dla_neck import DLANeck
 from .imvoxel_neck import IndoorImVoxelNeck, OutdoorImVoxelNeck
 from .pointnet2_fp_neck import PointNetFPNeck
 from .second_fpn import SECONDFPN
+from .convit3d_neck import ConViT3DNeck
 
 __all__ = [
     'FPN', 'SECONDFPN', 'OutdoorImVoxelNeck', 'PointNetFPNeck', 'DLANeck',
-    'IndoorImVoxelNeck'
+    'IndoorImVoxelNeck', 'ConViT3DNeck'
 ]
