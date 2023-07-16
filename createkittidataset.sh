9058506c8f3dcac340a4566a51ef63c73669721c
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-07-16 11:10:35 +0100

    creating kitti database script

---
+++ createkittidataset.sh
@@ -0,0 +1,6 @@
+wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/test.txt --no-check-certificate --content-disposition -O /workspace/data/kitti_detection/kitti/ImageSets/test.txt
+wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/train.txt --no-check-certificate --content-disposition -O /workspace/data/kitti_detection/kitti/ImageSets/train.txt
+wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/val.txt --no-check-certificate --content-disposition -O /workspace/data/kitti_detection/kitti/ImageSets/val.txt
+wget -c  https://raw.githubusercontent.com/traveller59/second.pytorch/master/second/data/ImageSets/trainval.txt --no-check-certificate --content-disposition -O /workspace/data/kitti_detection/kitti/ImageSets/trainval.txt
+
+python tools/create_data.py kitti --root-path /workspace/data/kitti_detection/kitti --out-dir /workspace/data/kitti_detection/kitti --extra-tag kitti
