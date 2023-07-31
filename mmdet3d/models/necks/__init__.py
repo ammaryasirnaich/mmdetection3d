58e68b91a1993923a5213e417a97a28a981a493a
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-07-31 15:05:59 +0100

    removing unwanted files

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -5,12 +5,11 @@ from .dla_neck import DLANeck
 from .imvoxel_neck import IndoorImVoxelNeck, OutdoorImVoxelNeck
 from .pointnet2_fp_neck import PointNetFPNeck
 from .second_fpn import SECONDFPN
-from .convit3d_neck import ConViT3DNeck
-from .convit3d_fullatt_neck import FullConViT3DNeck
+
 from .convit3d import VisionTransformer
 
 
 __all__ = [
     'FPN', 'SECONDFPN', 'OutdoorImVoxelNeck', 'PointNetFPNeck', 'DLANeck',
-    'IndoorImVoxelNeck', 'ConViT3DNeck','FullConViT3DNeck', 'VisionTransformer'
+    'IndoorImVoxelNeck', 'VisionTransformer'
 ]
