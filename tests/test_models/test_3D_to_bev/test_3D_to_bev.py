0ce44247627e2e678aaf1b32bf17e52d506fa97d
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-08-06 17:08:43 +0100

    working on test unit

--- tests/test_models/test_3D_to_bev/test_3D_to_bev.py
+++ tests/test_models/test_3D_to_bev/test_3D_to_bev.py
@@ -15,21 +15,21 @@ from mmdet3d.registry import MODELS
 
 
 
-@njit(parallel=True)
-def pointcloud_to_bev(point_cloud, resolution, range_x, range_y, output_shape):
-    num_pixels_x = output_shape[1]
-    num_pixels_y = output_shape[0]
-    bev = np.zeros((num_pixels_y, num_pixels_x), dtype=np.float32)
+# @njit(parallel=True)
+# def pointcloud_to_bev(point_cloud, resolution, range_x, range_y, output_shape):
+#     num_pixels_x = output_shape[1]
+#     num_pixels_y = output_shape[0]
+#     bev = np.zeros((num_pixels_y, num_pixels_x), dtype=np.float32)
 
-    for i in nb.prange(point_cloud.shape[0]):
-        x, y, z, intensity = point_cloud[i]
-        if range_x[0] <= x <= range_x[1] and range_y[0] <= y <= range_y[1]:
-            x_idx = int((x - range_x[0]) / resolution)
-            y_idx = int((y - range_y[0]) / resolution)
-            if 0 <= x_idx < num_pixels_x and 0 <= y_idx < num_pixels_y:
-                bev[y_idx, x_idx] = intensity
+#     for i in nb.prange(point_cloud.shape[0]):
+#         x, y, z, intensity = point_cloud[i]
+#         if range_x[0] <= x <= range_x[1] and range_y[0] <= y <= range_y[1]:
+#             x_idx = int((x - range_x[0]) / resolution)
+#             y_idx = int((y - range_y[0]) / resolution)
+#             if 0 <= x_idx < num_pixels_x and 0 <= y_idx < num_pixels_y:
+#                 bev[y_idx, x_idx] = intensity
 
-    return bev
+#     return bev
 
 
 
@@ -94,7 +94,7 @@ b,v,d = data.shape
 point_cloud =   data.reshape(b*v,d).numpy()
 
 # Convert point cloud to BEV
-bev = pointcloud_to_bev(point_cloud, resolution, range_x, range_y, output_shape)
+bev = kitti_to_bev(point_cloud, resolution, range_x, range_y, output_shape)
 
 # Plot the bird's eye view
 plt.imshow(bev, cmap='jet', extent=[range_x[0], range_x[1], range_y[0], range_y[1]], origin='lower')
--- tests/test_utils/test_3d_to_2d_bev.py
+++ tests/test_utils/test_3d_to_2d_bev.py
@@ -1,13 +1,12 @@
-import torch as t
+import numpy as t
 import numba
-# from numba import jit
+from numba import jit
 import matplotlib.pyplot as plt
+import torch as t
 
 # Numba JIT-compiled function for the projection
 # @jit(nopython=True)
-def bev_3D_to_2D(points_3d, resolution=0.1, height_range=(-3, 1), image_size=(800, 800)):
-
-    points_3d
+def project_to_birds_eye_view(points_3d, resolution=0.3, height_range=(-3, 1), image_size=(200, 200)):
     x_min = t.min(points_3d[:, 0])
     y_min = t.min(points_3d[:, 1])
     x_range = t.max(points_3d[:, 0]) - x_min
@@ -16,7 +15,7 @@ def bev_3D_to_2D(points_3d, resolution=0.1, height_range=(-3, 1), image_size=(80
     x_bins = int(x_range / resolution)
     y_bins = int(y_range / resolution)
 
-    hist = t.zeros((x_bins, y_bins, points_3d.shape[1] - 2), dtype=t.float32)
+    hist = t.zeros((x_bins, y_bins, points_3d.shape[1] - 2), dtype=t.float32, device="cuda")
 
     for point in points_3d:
         if height_range[0] <= point[2] <= height_range[1]:
@@ -27,13 +26,26 @@ def bev_3D_to_2D(points_3d, resolution=0.1, height_range=(-3, 1), image_size=(80
 
     return hist
 
-# # Generate a sample 3D point cloud with higher channel dimensions (XYZ + additional attributes)
+# Generate a sample 3D point cloud with higher channel dimensions (XYZ + additional attributes)
 # num_points = 1000
-# num_channels = 7  # Adjust the number of channels as needed
+num_channels = 4  # Adjust the number of channels as needed
 # point_cloud = t.random.rand(num_points, num_channels) * t.array([10, 10, 4, 1, 2, 3, 4])  # Adjust the scaling as needed
+
+point_cloud = t.load("/workspace/data/kitti_detection/pointclouds.pt")
+b,n,c = point_cloud.shape
+point_cloud = point_cloud.reshape(b*n,c)
+
+
+# print("point_cloud shape", point_cloud.shape)
+
+
 
 # # Project the point cloud to birds-eye view image
 # birdseye_view = project_to_birds_eye_view(point_cloud)
+
+# birdseye_view = birdseye_view.cpu().numpy()
+
+# print("birdseye_view shape", birdseye_view.shape)
 
 # # Plot the birds-eye view image for each channel
 # fig, axes = plt.subplots(1, num_channels - 2, figsize=(15, 3))
@@ -42,19 +54,18 @@ def bev_3D_to_2D(points_3d, resolution=0.1, height_range=(-3, 1), image_size=(80
 #     axes[i].set_title(f'Channel {i + 2}')
 # plt.tight_layout()
 # plt.show()
-
-
+# print("Pass")
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-def project_to_birds_eye_view_batch(point_clouds, resolution=0.1, height_range=(-2, 2), image_size=(800, 800)):
-    batch_size, num_points, num_channels = point_clouds.shape
+def project_to_birds_eye_view_batch(point_clouds_batch, resolution=0.1, height_range=(-2, 2), image_size=(200, 200)):
+    batch_size, num_points, num_channels = point_clouds_batch.shape
 
-    x_min = np.min(point_clouds[:, :, 0])
-    y_min = np.min(point_clouds[:, :, 1])
-    x_range = np.max(point_clouds[:, :, 0]) - x_min
-    y_range = np.max(point_clouds[:, :, 1]) - y_min
+    x_min = np.min(point_clouds_batch[:, :, 0])
+    y_min = np.min(point_clouds_batch[:, :, 1])
+    x_range = np.max(point_clouds_batch[:, :, 0]) - x_min
+    y_range = np.max(point_clouds_batch[:, :, 1]) - y_min
 
     x_bins = int(x_range / resolution)
     y_bins = int(y_range / resolution)
@@ -63,30 +74,37 @@ def project_to_birds_eye_view_batch(point_clouds, resolution=0.1, height_range=(
 
     for batch_idx in range(batch_size):
         for point_idx in range(num_points):
-            point = point_clouds[batch_idx, point_idx, :]
+            point = point_clouds_batch[batch_idx, point_idx, :]
             if height_range[0] <= point[2] <= height_range[1]:
                 x_index = int((point[0] - x_min) / resolution)
                 y_index = int((point[1] - y_min) / resolution)
                 if 0 <= x_index < x_bins and 0 <= y_index < y_bins:
-                    hist_batch[batch_idx, x_index, y_index, :] += point[3:]
+                    # Update each channel individually
+                    for channel_idx in range(3, num_channels - 1):  # Exclude XYZ and Intensity
+                        hist_batch[batch_idx, x_index, y_index, channel_idx - 3] += point[channel_idx]
 
     return hist_batch
 
-# Generate a batch of sample 3D point clouds with higher channel dimensions (XYZ + additional attributes)
+# Generate a batch of sample 3D point clouds with varying additional dimensions
 batch_size = 4
 num_points = 1000
-num_channels = 7  # Adjust the number of channels as needed
-point_clouds_batch = np.random.rand(batch_size, num_points, num_channels) * np.array([10, 10, 4, 1, 2, 3, 4])  # Adjust the scaling as needed
+num_channels = 10  # Adjust the number of channels as needed
+point_clouds_batch = np.random.rand(batch_size, num_points, num_channels) * np.array([10, 10, 4, 1, 2, 3, 4, 5, 6, 7])  # Adjust the scaling as needed
 
 # Project the point clouds to birds-eye view images
 birdseye_images_batch = project_to_birds_eye_view_batch(point_clouds_batch)
 
 # Plot the birds-eye view images for each batch and channel
 for batch_idx in range(batch_size):
-    for channel_idx in range(num_channels - 2):
+    for channel_idx in range(num_channels - 4):  # Exclude XYZ and Intensity
         plt.imshow(birdseye_images_batch[batch_idx, :, :, channel_idx], cmap='viridis', origin='upper')
         plt.xlabel('X')
         plt.ylabel('Y')
-        plt.title(f'Batch {batch_idx}, Channel {channel_idx + 2}')
+        plt.title(f'Batch {batch_idx}, Channel {channel_idx + 3}')
         plt.colorbar(label='Intensity')
         plt.show()
+        print("Pass")
+
+
+
+
