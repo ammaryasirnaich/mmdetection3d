dda32ddcfa4464e327d1633e35068b078961abf0
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-08-08 15:30:56 +0100

    refactoring the backbone configuration and batch size

--- configs/3dconvit/conViT3D_Pointrpn_head.py
+++ configs/3dconvit/conViT3D_Pointrpn_head.py
@@ -91,7 +91,7 @@ eval_pipeline = [
 
 
 train_dataloader = dict(
-    batch_size=8,
+    batch_size=4,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
@@ -192,33 +192,53 @@ model = dict(
     data_preprocessor=dict(type='Det3DDataPreprocessor'),
     voxel_encoder =None,
     middle_encoder =None,
+    
     backbone=dict(
-        type='PointNet2SAMSG',
-        in_channels=4,
-        num_points=(4096, 1024, 256, 64),
-        radii=((0.1, 0.5), (0.5, 1.0), (1.0, 2.0), (2.0, 4.0)),
-        num_samples=((16, 32), (16, 32), (16, 32), (16, 32)),
-        sa_channels=(((16, 16, 32), (32, 32, 64)), ((64, 64, 128), (64, 96,
-                                                                    128)),
-                     ((128, 196, 256), (128, 196, 256)), ((256, 256, 512),
-                                                          (256, 384, 512))),
-        fps_mods=(('D-FPS'), ('D-FPS'), ('D-FPS'), ('D-FPS')),
-        fps_sample_range_lists=((-1), (-1), (-1), (-1)),
-        aggregation_channels=(None, None, None, None),
-        dilated_group=(False, False, False, False),
-        out_indices=(0, 1, 2, 3),
-        norm_cfg=dict(type='BN2d', eps=1e-3, momentum=0.1),
-        sa_cfg=dict(
-            type='PointSAModuleMSG',
-            pool_mod='max',
-            use_xyz=True,
-            normalize_xyz=False)),
+            type='PointNet2SAMSG',
+            in_channels=4,
+            num_points=(4096, 1024, (256, 256)),
+            radii=((0.2, 0.4, 0.8), (0.4, 0.8, 1.6), (1.6, 3.2, 4.8)),
+            num_samples=((32, 32, 64), (32, 32, 64), (32, 32, 32)),
+            sa_channels=(((16, 16, 32), (16, 16, 32), (32, 32, 64)),
+                        ((64, 64, 128), (64, 64, 128), (64, 96, 128)),
+                        ((128, 128, 256), (128, 192, 256), (128, 256, 256))),
+            aggregation_channels=(64, 128, 256),
+            fps_mods=(('D-FPS'), ('FS'), ('F-FPS', 'D-FPS')),
+            fps_sample_range_lists=((-1), (-1), (512, -1)),
+            norm_cfg=dict(type='BN2d', eps=1e-3, momentum=0.1),
+            sa_cfg=dict(
+                type='PointSAModuleMSG',
+                pool_mod='max',
+                use_xyz=True,
+                normalize_xyz=False)),
+
+    #    backbone=dict(
+    #     type='PointNet2SAMSG',
+    #     in_channels=4,
+    #     num_points=(4096, 1024, 256, 64),
+    #     radii=((0.1, 0.5), (0.5, 1.0), (1.0, 2.0), (2.0, 4.0)),
+    #     num_samples=((16, 32), (16, 32), (16, 32), (16, 32)),
+    #     sa_channels=(((16, 16, 32), (32, 32, 64)), ((64, 64, 128), (64, 96,
+    #                                                                 128)),
+    #                  ((128, 196, 256), (128, 196, 256)), ((256, 256, 512),
+    #                                                       (256, 384, 512))),
+    #     fps_mods=(('D-FPS'), ('D-FPS'), ('D-FPS'), ('D-FPS')),
+    #     fps_sample_range_lists=((-1), (-1), (-1), (-1)),
+    #     aggregation_channels=(None, None, None, None),
+    #     dilated_group=(False, False, False, False),
+    #     out_indices=(0, 1, 2, 3),
+    #     norm_cfg=dict(type='BN2d', eps=1e-3, momentum=0.1),
+    #     sa_cfg=dict(
+    #         type='PointSAModuleMSG',
+    #         pool_mod='max',
+    #         use_xyz=True,
+    #         normalize_xyz=False)),
 
       neck =  dict(
                 type='VisionTransformer',   
                 num_classes=3, 
                 # in_chans=256, #1024
-                embed_dim=1024, #1024
+                embed_dim=256, #1024
                 depth = 12, #  Depths Transformer stage. Default 12
                 num_heads=8 ,  # 12
                 mlp_ratio=4,
