95e041a31fcb9470fb1dae62b14da0d1b59fa895
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-09-06 19:53:44 +0100

    finding bug

--- mmdet3d/visualization/get3dInstancefrompkl.py
+++ mmdet3d/visualization/get3dInstancefrompkl.py
@@ -7,34 +7,20 @@ from mmengine.structures import InstanceData
 from mmdet3d.structures import (BaseInstance3DBoxes, Box3DMode, CameraInstance3DBoxes)
                               
 
-## for single object displya
-
-# info_file = load('demo/data/kitti/kitti_infos_test.pkl')   # ['metainfo', 'data_list'
-# info_file = load('demo/data/kitti/kitti_infos_train.pkl')   # ['metainfo', 'data_list'
-
-
 def get_3dInstance_from_pklfile(lidarfileinstance:str):
     # test_pkl_file = '/workspace/data/kitti_detection/kitti/kitti_infos_test.pkl'
     # test_pkl_file = '/workspace/data/kitti_detection/kitti/kitti_infos_train.pkl'
     val_pkl_file = '/workspace/data/kitti_detection/kitti/kitti_infos_val.pkl'
     info_file = load(val_pkl_file)   # ['metainfo', 'data_list'
-     
-
-    # Traverse the data to extract 3D bounding box information
-    # print(info_file['data_list'][0].keys()) 
-
-    
+        
     bboxes_3d = []
     labels_3d = []
     gt_instances_3d = InstanceData()
     lidar2cam = np.array(info_file['data_list'][0]['images']['CAM2']['lidar2cam'])
 
-    ### sample_idx', 'images', 'lidar_points', 'instances', 'cam_instances
+
     for info_dic in info_file['data_list']:
-            # print(type(info_dic))
             lidar_info_str= info_dic['lidar_points']['lidar_path']
-            # print(type(lidar_info_dic))
-            # print(lidar_info_str) 
             if(lidar_info_str == lidarfileinstance):
                 if(len(info_dic['instances'])==0):
                      print("No instance found")
@@ -45,10 +31,11 @@ def get_3dInstance_from_pklfile(lidarfileinstance:str):
                     labels_3d.append(instance_dic['bbox_label_3d'])
 
                 bbox3d = torch.tensor(bboxes_3d)
-                labels_3d = torch.tensor(labels_3d)  
-                gt_instances_3d.bboxes_3d = CameraInstance3DBoxes(bbox3d).convert_to(Box3DMode.LIDAR,
-                                                 np.linalg.inv(lidar2cam))
-                gt_instances_3d.labels_3d = labels_3d
+                # gt_instances_3d.bboxes_3d = CameraInstance3DBoxes(bbox3d).convert_to(Box3DMode.LIDAR,
+                #                     np.linalg.inv(lidar2cam))
+                gt_instances_3d.bboxes_3d = CameraInstance3DBoxes(bbox3d).convert_to(Box3DMode.LIDAR)
+          
+                gt_instances_3d.labels_3d = torch.tensor(labels_3d) 
                 return  gt_instances_3d
 
           
