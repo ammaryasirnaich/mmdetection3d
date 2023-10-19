36739a014d99161a22e738132e32def45b52752d
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-10-19 15:31:07 +0100

    updating the script for single scene inference for demo

--- pcd_demo.sh
+++ pcd_demo.sh
@@ -1,10 +1,22 @@
 
-CONFIG_FILE="/workspace/mmdetection3d/work_dirs/convit3d_PointNet_Transformer_rpnhead/conViT3D_Pointrpn_head.py"
-CHECKPOINT_FILE="/workspace/mmdetection3d/work_dirs/convit3d_PointNet_Transformer_rpnhead/epoch_41.pth"
+# CONFIG_FILE="/workspace/mmdetection3d/work_dirs/convit3d_PointNet_Transformer_rpnhead/conViT3D_Pointrpn_head.py"
+# CHECKPOINT_FILE="/workspace/mmdetection3d/work_dirs/convit3d_PointNet_Transformer_rpnhead/epoch_41.pth"
+
+
+CONFIG_FILE="/workspace/mmdetection3d/configs/3dconvit/boundingBox_on_convit3D.py"
+CHECKPOINT_FILE="/workspace/mmdetection3d/work_dirs/convit3D_PointNet_transformer_ssdhead__14_August/epoch_202.pth"
+
+
+
+
+
 PCD_FILE="/workspace/mmdetection3d/demo/data/kitti/000008.bin"
 
 
+
+
 # pwd 
-python demo/pcd_demo.py ${PCD_FILE} ${CONFIG_FILE} ${CHECKPOINT_FILE} 
+# python demo/pcd_demo.py ${PCD_FILE} ${CONFIG_FILE} ${CHECKPOINT_FILE} 
+python tests/intensity_mode_visualization.py ${PCD_FILE} ${CONFIG_FILE} ${CHECKPOINT_FILE} 
 
 
---
+++ tests/intensity_mode_visualization.py
@@ -0,0 +1,78 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from argparse import ArgumentParser
+
+from mmdet3d.apis import inference_detector, init_model
+from mmdet3d.registry import VISUALIZERS
+from mmdet3d.visualization.get3dInstancefrompkl import *
+from mmdet3d.structures import Det3DDataSample
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('pcd', help='Point cloud file')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--score-thr', type=float, default=0.1, help='bbox score threshold')
+    parser.add_argument(
+        '--out-dir', type=str, default='demo', help='dir to save results')
+    parser.add_argument(
+        '--show',
+        action='store_true',
+        help='show online visualization results')
+    parser.add_argument(
+        '--snapshot',
+        action='store_true',
+        help='whether to save online visualization results')
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    # TODO: Support inference of point cloud numpy file.
+    # build the model from a config file and a checkpoint file
+    model = init_model(args.config, args.checkpoint, device=args.device)
+
+    # init visualizer
+    visualizer = VISUALIZERS.build(model.cfg.visualizer)
+    visualizer.dataset_meta = model.dataset_meta
+
+    # test a single point cloud sample
+    result, data = inference_detector(model, args.pcd)
+    points = data['inputs']['points']
+    data_input = dict(points=points)
+    
+    # lidarinstanceName= '000008.bin'
+    
+    lidarinstanceName = args.pcd
+    lidarinstanceName = lidarinstanceName.split("/")[-1]
+
+    gt_instances_3d = get_3dInstance_from_pklfile(lidarinstanceName)
+
+
+    # gt_det3d_data_sample = Det3DDataSample()
+    # gt_det3d_data_sample.gt_instances_3d = gt_instances_3d
+    result.gt_instances_3d = gt_instances_3d
+    
+  
+    
+    
+
+    # show the results
+    visualizer.add_datasample(
+        'result',
+        data_input,
+        data_sample=result,
+        draw_gt=True,
+        show=args.show,
+        wait_time=-1,
+        out_file=args.out_dir,
+        pred_score_thr=args.score_thr,
+        vis_task='lidar_det')
+    
+    visualizer.show()
+    
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
