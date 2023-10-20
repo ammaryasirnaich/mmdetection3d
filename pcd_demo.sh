241eb9fdb66c4719d560e9953431727b21413140
Ammar Yasir Naich <ammar.naich@gmail.com>
2023-10-20 11:00:57 +0100

    updating the script files

--- pcd_demo.sh
+++ pcd_demo.sh
@@ -2,17 +2,25 @@
 # CONFIG_FILE="/workspace/mmdetection3d/work_dirs/convit3d_PointNet_Transformer_rpnhead/conViT3D_Pointrpn_head.py"
 # CHECKPOINT_FILE="/workspace/mmdetection3d/work_dirs/convit3d_PointNet_Transformer_rpnhead/epoch_41.pth"
 
-
-CONFIG_FILE="/workspace/mmdetection3d/configs/3dconvit/boundingBox_on_convit3D.py"
-CHECKPOINT_FILE="/workspace/mmdetection3d/work_dirs/convit3D_PointNet_transformer_ssdhead__14_August/epoch_202.pth"
-
-
-
-
-
 PCD_FILE="/workspace/mmdetection3d/demo/data/kitti/000008.bin"
 
+# CONFIG_FILE="/workspace/mmdetection3d/configs/3dconvit/boundingBox_on_convit3D.py"
+# CHECKPOINT_FILE="/workspace/mmdetection3d/work_dirs/convit3D_PointNet_transformer_ssdhead__14_August/epoch_202.pth"
 
+
+CONFIG_FILE="/workspace/mmdetection3d/configs/inet/intensityNetKitti_all_objects.py"
+CHECKPOINT_FILE="/workspace/data/kitti_detection/IVEF/epoch_80.pth"
+
+
+
+
+# CONFIG_FILE="/workspace/mmdetection3d/configs/pointpillars/pointpillars_hv_secfpn_8xb6-160e_kitti-3d-car.py"
+# CHECKPOINT_FILE="/workspace/data/kitti_detection/models_to_test/pointpiller/hv_pointpillars_secfpn_6x8_160e_kitti-3d-3class_20220301_150306-37dc2420.pth"
+
+
+
+# CONFIG_FILE="/workspace/mmdetection3d/configs/second/second_hv_secfpn_8xb6-amp-80e_kitti-3d-car.py"
+# CHECKPOINT_FILE="/workspace/data/kitti_detection/models_to_test/second/hv_second_secfpn_fp16_6x8_80e_kitti-3d-3class_20200925_110059-05f67bdf.pth"
 
 
 # pwd 
--- training_script.sh
+++ training_script.sh
@@ -3,7 +3,7 @@
 # python tools/train.py /workspace/mmdetection3d/configs/3dconvit/convit3D_no_voxel_no_transformer_ssdhead.py
 # python tools/train.py /workspace/mmdetection3d/configs/3dconvit/convit3D_pointnet_transformer_ssdhead.py
 
-python tools/train.py /workspace/mmdetection3d/configs/3dconvit/convit3D_pointnet_transformer_ssdhead.py
+# python tools/train.py /workspace/mmdetection3d/configs/3dconvit/convit3D_pointnet_transformer_ssdhead.py
 
 # python tools/train.py /workspace/mmdetection3d/configs/3dconvit/convit3D_smallPointNet_transformer_ssdhead.py
 
@@ -12,6 +12,10 @@ python tools/train.py /workspace/mmdetection3d/configs/3dconvit/convit3D_pointne
 
 # python tools/train.py /workspace/mmdetection3d/configs/3dconvit/conViT3D_PointRCNN_head.py
 # python tools/train.py /workspace/mmdetection3d/configs/3dssd/3dssd_4xb4_kitti-3d-all.py
+
+
+
+python tools/train.py /workspace/mmdetection3d/configs/inet/intensityNetKitti_all_objects.py
 
 
 
