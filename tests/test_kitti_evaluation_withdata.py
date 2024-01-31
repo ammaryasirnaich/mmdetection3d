ad683f9c090fafdaa351c1fa7309e76012b109ca
Ammar Yasir Naich <ammar.naich@gmail.com>
2024-01-31 13:45:02 +0000

    evaluation code is working

--- tests/test_kitti_evaluation_withdata.py
+++ tests/test_kitti_evaluation_withdata.py
@@ -33,26 +33,23 @@ def _init_evaluate_input(result_dict,anno_path):
     metrics= ['bbox']
     gt_annos=[]
     
-    gt_annos_temp =[]
-    
 
-
-    for result in result_dict:
-        # sample = result['sample_idx'][0]
-                
-        if(len(result['sample_idx'])>0 and int(result['sample_idx'][0])< len(data_infos)):
-            sample = result['sample_idx'][0]
-            # print(sample)
-            
-            kitti_anno = data_infos[sample]['kitti_annos']
-            gt_annos_temp.append(kitti_anno)
+    # gt_annos_temp =[]
+    # for result in result_dict['pred_instances_3d']:    
+    #     if(len(result['sample_idx'])>0 and int(result['sample_idx'][0])< len(data_infos)):
+    #         sample = result['sample_idx'][0]
+    #         print(sample)
+    #         kitti_anno = data_infos[sample]['kitti_annos']
+    #         gt_annos_temp.append(kitti_anno)
             
     
-        gt_annos = [
+    gt_annos = [
         data_infos[result['sample_idx'][0]]['kitti_annos']
-        for result in result_dict
-    ]
-    
+        for result in result_dict['pred_instances_3d']
+    ]    
+    # print('result_dict:',len(result_dict['pred_instances_3d']))
+    # print('data_infos:',len(data_infos))
+    # print('gt_annos length',len(gt_annos))
  
     for metric in metrics:
         ap_dict = kittimetric.kitti_evaluate(
@@ -68,44 +65,6 @@ def _init_evaluate_input(result_dict,anno_path):
 
 
 
-def dumpingObjects():
-    
-    ann_file = '/workspace/data/kitti_detection/model_output_results/predictionresults'
-    # ann_file = osp.dirname(rootPath)+"/test.json"
-    
-    results = {"alpha": 1, "beta": 2}   
-
-    
-    path = osp.dirname(ann_file)+"/test.json"
-    with open(path, 'w') as fout:
-        # json.dump(results, fout)
-        metainfo = dict(sample_idx=0)
-        predictions = Det3DDataSample()
-        pred_instances_3d = InstanceData()
-        pred_instances = InstanceData()
-        pred_instances.bboxes = torch.tensor([[712.4, 143, 810.7, 307.92]])
-        pred_instances.scores = torch.Tensor([0.9])
-        pred_instances.labels = torch.Tensor([0])
-        pred_instances_3d.bboxes_3d = LiDARInstance3DBoxes(
-            torch.tensor(
-                [[8.7314, -1.8559, -1.5997, 0.4800, 1.2000, 1.8900, 0.0100]]))
-
-        pred_instances_3d.scores_3d = torch.Tensor([0.9])
-        pred_instances_3d.labels_3d = torch.Tensor([0])
-
-        predictions.pred_instances_3d = pred_instances_3d
-        predictions.pred_instances = pred_instances
-        predictions.set_metainfo(metainfo)
-        predictions = predictions.to_dict()
-        
-        encoded_data = jsonpickle.encode(predictions, unpicklable=False)
-        json.dump(encoded_data, fout)
-
-
-
-
-
-
 def test_kitti_metric_mAP():
     if not torch.cuda.is_available():
         pytest.skip('test requires GPU and torch+cuda')
@@ -115,7 +74,7 @@ def test_kitti_metric_mAP():
     info_path = data_root + '/kitti_infos_val.pkl'
     
     # pred_result_file = '/workspace/data/pred_instances_3d.pkl'
-    pred_result_file = '/workspace/data/kitti_detection/model_output_results/prediction_3d_mmdet3_format.pk'
+    pred_result_file = '/workspace/data/kitti_detection/model_output_results/prediction_3d_mmdet3_format.pkl'
     result_dict =load(pred_result_file)
  
     predictions = _init_evaluate_input(result_dict,info_path)
