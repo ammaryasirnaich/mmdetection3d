b0e4d0449d2e9f32d4fe0fee47aadb379b8f7f00
ammr yasir <ammar.naich@gmail.com>
2024-05-07 18:05:20 +0100

    pushing features

--- training_script.sh
+++ training_script.sh
@@ -14,8 +14,10 @@
 # python tools/train.py /workspace/mmdetection3d/configs/3dconvit/conViT3D_PointRCNN_head.py
 # python tools/train.py /workspace/mmdetection3d/configs/3dssd/3dssd_4xb4_kitti-3d-all.py
 # python tools/train.py /workspace/mmdetection3d/configs/3dssd/3dssd_4xb4_kitti-3d-all.py
-python tools/train.py /workspace/mmdetection3d/configs/inet/intensityNetKitti_all_objects.py
 
+CUDA_VISIBLE_DEVICES=1 python  tools/train.py /import/digitreasure/ammar_workspace/mmdetection3d/configs/3dconvit/convit3D_pointNet_transformer_nusence.py
+
+# CUDA_VISIBLE_DEVICES=1 python  tools/train.py "/import/digitreasure/ammar_workspace/mmdetection3d/configs/pointpillars/pointpiller_nusense_custome.py"
 
 # python tools/train.py /workspace/mmdetection3d/configs/inet/inet_kitti_3d_all_update.py
 # python tools/train.py /workspace/mmdetection3d/configs/3dconvit/convit3D_pointNet_transformer_ssdhead_configInherence.py
