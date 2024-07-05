404f4cdcf132a5182abc539ff76e62d87e00a3b2
Ammar Yasir Naich <ammar.naich@gmail.com>
2024-07-05 17:16:10 +0100

    update the inference

--- tests/test_models/mode_inference_test.py
+++ tests/test_models/mode_inference_test.py
@@ -6,6 +6,9 @@ from mmdet3d.visualization import Det3DLocalVisualizer
 import open3d as o3d
 import matplotlib.pyplot as plt
 
+from scipy.stats import norm 
+import statistics 
+
 
 def get_attention_map():  
     rootpath="/workspace/data/kitti_detection/model_output_results/convit3D_kitti_24_June_2024/"
@@ -90,34 +93,35 @@ def get_normlized_dimensions(attention_maps):
     return norm_dim
 
 
-
 def visualize_maps(attention_map_info,raw_pointcloud):
-   
+    
     
     attention_map = attention_map_info['attention_map'].cpu().numpy().squeeze(0)
     attention_points = attention_map_info['points'].cpu().numpy().squeeze(0)
    
-    point_attention = np.concatenate((attention_points,attention_map),axis=1)
+    # point_attention = np.concatenate((attention_points,attention_map),axis=1)
     # print("shape of point attention:", point_attention.shape)
     # np.savez('poin_attention',point_attention)
-    np.savez('raw_pointclouds',raw_pointcloud)
+    # np.savez('raw_pointclouds',raw_pointcloud)
    
     # 'plasma', 'viridis'
-    pcd_raw = get_pointcloud(raw_pointcloud,"None")   #"plasma"
-    pcd_atten = get_pointcloud(attention_points,"None")
+
+    # pcd_atten = get_pointcloud(attention_points,"None")
     # vis.draw_geometries([get_pointcloud(points)], window_name="Attention Map Visualization")
    
    
    # Set colors based on attention weights
     attention_weights = get_normlized_dimensions(attention_map)
-    print("attention_weights:", attention_weights.shape)
     weight_colors = plt.get_cmap("hot")(attention_weights)[:, :3]  # Use a colormap to map attention weights to colors
-    print(weight_colors.shape)
-    pcd_atten.colors = o3d.utility.Vector3dVector(weight_colors)
+    print("attention_weights:", attention_weights.shape)
+    print("attentn color:",weight_colors.shape)
+    # pcd_atten.colors = o3d.utility.Vector3dVector(weight_colors)
+    
+    # plot_gaussian(weight_colors)
     
     # Create a list of spheres for each point to visualize their sizes
     spheres = []
-    min_size = 0.1
+    min_size = 0.01
     max_size = 0.5
     point_sizes = min_size + (max_size - min_size) * attention_weights
 
@@ -128,27 +132,36 @@ def visualize_maps(attention_map_info,raw_pointcloud):
         spheres.append(sphere)
 
     # Combine all spheres into a single geometry
-    combined_mesh = o3d.geometry.TriangleMesh()
+    attnt_points = o3d.geometry.TriangleMesh()
     for sphere in spheres:
-        combined_mesh += sphere
+        attnt_points += sphere
 
-
-    
-    nor_dim = get_normlized_dimensions(raw_pointcloud)
-    raw_pnt_colors = plt.get_cmap("binary")(nor_dim)[:, :3]  # Use a colormap to map attention weights to colors
-    print(raw_pnt_colors.shape)
+    pcd_raw = get_pointcloud(raw_pointcloud,"None")   #"plasma"
+    raw_pnt_nor_dim = get_normlized_dimensions(raw_pointcloud)
+    raw_pnt_colors = plt.get_cmap("binary")(raw_pnt_nor_dim)[:, :3]  # Use a colormap to map attention weights to colors
     pcd_raw.colors = o3d.utility.Vector3dVector(raw_pnt_colors)
     
     vis = o3d.visualization.Visualizer()
-    
-    
+
     vis.create_window()
     vis.add_geometry(pcd_raw)
-    vis.add_geometry(pcd_atten)
-    
-    vis.add_geometry(combined_mesh)
-
+    # vis.add_geometry(pcd_atten)
+    vis.add_geometry(attnt_points)
     vis.get_render_option().point_size = 2
+    
+    # # Plot the color bar using Matplotlib
+    # fig, ax = plt.subplots(figsize=(6, 1))
+    # fig.subplots_adjust(bottom=0.5)
+    # cmap = plt.get_cmap("hot")
+    # # Create a color bar based on the colormap
+    # norm = plt.Normalize(vmin=weight_colors.min(), vmax=weight_colors.max())
+    # cbar = fig.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax, orientation='horizontal')
+    # cbar.set_label('Normalized Attention Weights')
+
+    # plt.show()
+    
+    
+    
     vis.run()
     
     
@@ -162,7 +175,11 @@ def testingsizingpointclouds():
 
     # Calculate weight colors
     weight_colors = plt.get_cmap("hot")(attention_weights)[:, :3]
+    
+    cmap = plt.get_cmap("hot")
+    color = cmap(attention_weights)
 
+    
     # Resize points based on attention weights
     # Normalize the attention weights to get sizes in a reasonable range for visualization
     min_size = 0.01
@@ -191,6 +208,32 @@ def testingsizingpointclouds():
     o3d.visualization.draw_geometries([combined_mesh])
     o3d.visualization.get_render_option().point_size = 2
 
+
+def plot_gaussian(attention_weights):
+   
+    # Normalize the attention weights to have a mean of 0 and standard deviation of 1
+    mean_weight = np.mean(attention_weights)
+    std_weight = np.std(attention_weights)
+    normalized_weights = (attention_weights - mean_weight) / std_weight
+
+    # Fit a Gaussian distribution
+    mu, sigma = norm.fit(normalized_weights)
+
+    # Generate x values for plotting the Gaussian distribution
+    x = np.linspace(min(normalized_weights), max(normalized_weights), 1000)
+
+    # Calculate the Gaussian distribution values
+    gaussian_distribution = norm.pdf(x, mu, sigma)
+
+    # Plot the Gaussian distribution
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, gaussian_distribution, label=f'Gaussian fit: $\mu$={mu:.2f}, $\sigma$={sigma:.2f}')
+    plt.hist(normalized_weights, bins=30, density=True, alpha=0.6, color='g', label='Histogram of attention weights')
+    plt.title('Gaussian Distribution Fitted to Attention Weights')
+    plt.xlabel('Value')
+    plt.ylabel('Probability Density')
+    plt.legend()
+    plt.show()
 
 def main():
     attention_map_info,raw_pointclouds =get_attention_map()
