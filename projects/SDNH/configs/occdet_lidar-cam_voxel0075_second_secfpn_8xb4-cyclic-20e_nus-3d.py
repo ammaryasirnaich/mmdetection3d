54c37fa93d579985aaa8eaa2d726fa7ea5189426
ammr yasir <ammar.naich@gmail.com>
2024-08-13 18:26:25 +0000

    update the repo

--- projects/SDNH/configs/occdet_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
+++ projects/SDNH/configs/occdet_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
@@ -1,12 +1,12 @@
 _base_ = [
-    './bevfusion_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py'
+    './occdet_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py'
 ]
 point_cloud_range = [-54.0, -54.0, -5.0, 54.0, 54.0, 3.0]
 input_modality = dict(use_lidar=True, use_camera=True)
 backend_args = None
 
 model = dict(
-    type='BEVFusion',
+    type='OccupancyDetector',
     data_preprocessor=dict(
         type='Det3DDataPreprocessor',
         mean=[123.675, 116.28, 103.53],
