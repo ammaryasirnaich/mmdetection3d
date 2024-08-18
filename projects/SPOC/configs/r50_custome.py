fa5ff815b5f58882880285d1a51a8e778a40a0e7
ammr yasir <ammar.naich@gmail.com>
2024-08-18 01:56:38 +0000

    updating config file

--- projects/SPOC/configs/r50_custome.py
+++ projects/SPOC/configs/r50_custome.py
@@ -1,11 +1,9 @@
-dataset_type = 'NuSceneOcc'
-dataset_root = '/import/digitreasure/openmm_processed_dataset/nusense_dataset/nuscenses/'
-occ_gt_root = '/import/digitreasure/openmm_processed_dataset/nusense_dataset/nuscenses/occ3d'
 
-
-custom_imports = dict(imports=['projects.SPOC.models'],allow_failed_imports=False)
-
-backend_args = None
+# _base_ = [
+#     '../../../configs/_base_/datasets/nus-3d.py',
+#     '../../../configs/_base_/default_runtime.py',
+#     '../../../configs/_base_/schedules/cyclic-20e.py'
+# ]
 
 
 # If point cloud range is changed, the models should also change their point
@@ -33,6 +31,25 @@ occ_class_names = [
     'terrain', 'manmade', 'vegetation', 'free'
 ]
 
+
+metainfo = dict(classes=det_class_names)
+dataset_type = 'NuSceneOcc'
+dataset_root = '/import/digitreasure/openmm_processed_dataset/nusense_dataset/nuscenses/'
+occ_gt_root = '/import/digitreasure/openmm_processed_dataset/nusense_dataset/nuscenses/occ3d'
+backend_args = None
+
+data_prefix = dict(
+    pts='samples/LIDAR_TOP',
+    CAM_FRONT='samples/CAM_FRONT',
+    CAM_FRONT_LEFT='samples/CAM_FRONT_LEFT',
+    CAM_FRONT_RIGHT='samples/CAM_FRONT_RIGHT',
+    CAM_BACK='samples/CAM_BACK',
+    CAM_BACK_RIGHT='samples/CAM_BACK_RIGHT',
+    CAM_BACK_LEFT='samples/CAM_BACK_LEFT',
+    sweeps='sweeps/LIDAR_TOP')
+
+
+
 input_modality = dict(
     use_lidar=False,
     use_camera=True,
@@ -49,6 +66,9 @@ _num_frames_ = 8
 _num_queries_ = 100
 _topk_training_ = [4000, 16000, 64000]
 _topk_testing_ = [2000, 8000, 32000]
+
+custom_imports = dict(imports=['projects.SPOC.models'],allow_failed_imports=False)
+
 
 model = dict(
     type='SparseOcc',
@@ -90,6 +110,45 @@ bda_aug_conf = dict(
     flip_dy_ratio=0.5
 )
 
+
+
+db_sampler = dict(
+    data_root=dataset_root,
+    info_path=dataset_root + 'nuscenes_dbinfos_train.pkl',
+    rate=1.0,
+    prepare=dict(
+        filter_by_difficulty=[-1],
+        filter_by_min_points=dict(
+            car=5,
+            truck=5,
+            bus=5,
+            trailer=5,
+            construction_vehicle=5,
+            traffic_cone=5,
+            barrier=5,
+            motorcycle=5,
+            bicycle=5,
+            pedestrian=5)),
+    classes=det_class_names,
+    sample_groups=dict(
+        car=2,
+        truck=3,
+        construction_vehicle=7,
+        bus=4,
+        trailer=6,
+        barrier=2,
+        motorcycle=6,
+        bicycle=6,
+        pedestrian=2,
+        traffic_cone=2),
+    points_loader=dict(
+        type='LoadPointsFromFile',
+        coord_type='LIDAR',
+        load_dim=5,
+        use_dim=[0, 1, 2, 3, 4],
+        backend_args=backend_args))
+
+
 train_pipeline = [
     dict(type='LoadMultiViewImageFromFiles', to_float32=False, color_type='color'),
     dict(type='LoadMultiViewImageFromMultiSweeps', sweeps_num=_num_frames_ - 1),
@@ -120,16 +179,19 @@ batch_size = 8
 train_dataloader = dict(
     batch_size=batch_size,  # Set appropriate batch size
     num_workers=8,
-    shuffle=True,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
         type=dataset_type,
         data_root=dataset_root,
         occ_gt_root=occ_gt_root,
         ann_file=dataset_root + 'nuscenes_infos_train_sweep.pkl',
         pipeline=train_pipeline,
-        classes=det_class_names,
+        metainfo=metainfo,
         modality=input_modality,
         test_mode=False,
+        data_prefix=data_prefix,
+        use_valid_flag=True,
     ),
 )
 
@@ -144,9 +206,11 @@ val_dataloader = dict(
         occ_gt_root=occ_gt_root,
         ann_file=dataset_root + 'nuscenes_infos_val_sweep.pkl',
         pipeline=test_pipeline,
-        classes=det_class_names,
+        metainfo=metainfo,
         modality=input_modality,
+        data_prefix=data_prefix,
         test_mode=True,
+        backend_args=backend_args
     ),
 )
 
@@ -154,7 +218,9 @@ val_dataloader = dict(
 test_dataloader = dict(
     batch_size=4,  # Set appropriate batch size
     num_workers=8,
-    shuffle=False,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type=dataset_type,
         data_root=dataset_root,
@@ -163,6 +229,7 @@ test_dataloader = dict(
         pipeline=test_pipeline,
         classes=det_class_names,
         modality=input_modality,
+        data_prefix=data_prefix,
         test_mode=True,
     ),
 )
@@ -183,31 +250,65 @@ val_cfg = dict()
 test_cfg = dict()
 
 
+# Optimizer Configuration
+optim_wrapper = dict(
+    optimizer=dict(
+        type='AdamW',
+        lr=5e-4,
+        weight_decay=0.01,
+        paramwise_cfg=dict(
+            custom_keys={
+                'img_backbone': dict(lr_mult=0.1),
+                'sampling_offset': dict(lr_mult=0.1),})),
+    clip_grad=dict(max_norm=35,norm_type=2) )
 
-optimizer = dict(
-    type='AdamW',
-    lr=5e-4,
-    paramwise_cfg=dict(
-        custom_keys={
-            'img_backbone': dict(lr_mult=0.1),
-            'sampling_offset': dict(lr_mult=0.1),
-        }),
-    weight_decay=0.01
-)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=1.0 / 3,
-    by_epoch=True,
-    step=[22, 24],
-    gamma=0.2
-)
+param_scheduler = [
+    # Warmup Configuration
+    dict(
+        type='LinearLR',  # Linear warmup scheduler
+        start_factor=1.0 / 3,
+        by_epoch=True,
+        begin=0,
+        end=500,  # Equivalent to warmup_iters=500
+        convert_to_iter_based=True  # Converts to iterations if by_epoch=True
+    ),
+    # Step LR Scheduler
+    dict(
+        type='MultiStepLR',
+        by_epoch=True,
+        milestones=[22, 24],  # Equivalent to step=[22, 24]
+        gamma=0.2  # Learning rate decay factor
+    )
+]
+
+
+# optimizer = dict(
+#     type='AdamW',
+#     lr=5e-4,
+#     paramwise_cfg=dict(
+#         custom_keys={
+#             'img_backbone': dict(lr_mult=0.1),
+#             'sampling_offset': dict(lr_mult=0.1),
+#         }),
+#     weight_decay=0.01
+# )
+# optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+
+# lr_config = dict(
+#     policy='step',
+#     warmup='linear',
+#     warmup_iters=500,
+#     warmup_ratio=1.0 / 3,
+#     by_epoch=True,
+#     step=[22, 24],
+#     gamma=0.2
+# )
+
+
 total_epochs = 24
 
-
+auto_scale_lr = dict(enable=False, base_batch_size=32)
 # load pretrained weights
 load_from = 'pretrain/cascade_mask_rcnn_r50_fpn_coco-20e_20e_nuim_20201009_124951-40963960.pth'
 revise_keys = [('backbone', 'img_backbone')]
