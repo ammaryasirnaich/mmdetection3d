952ff43de08cb92dc13b8783776bc38c6f104ded
ammr yasir <ammar.naich@gmail.com>
2024-10-09 16:47:42 +0000

    remove the sdf directory

--- projects/SDF/models/losses.py
+++
@@ -1,7 +0,0 @@
-# src/losses.py
-import torch
-
-def combined_loss(task_loss, sparse_weight, dense_weight, lambda_reg=0.1):
-    reg_loss = lambda_reg * torch.sum(torch.abs(sparse_weight - dense_weight))
-    total_loss = task_loss + reg_loss
-    return total_loss
