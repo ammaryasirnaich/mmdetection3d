489c7ac4608d9992efb1be4878672117635e36d4
Ammar Yasir Naich <ammar.naich@gmail.com>
2024-10-14 01:03:36 +0100

    updating the depth class

--- projects/EfficientFusionDetector/model/__init__.py
+++ projects/EfficientFusionDetector/model/__init__.py
@@ -7,6 +7,7 @@ from .transformer import TransformerDecoderLayer
 from .transforms_3d import (BEVFusionGlobalRotScaleTrans,
                             BEVFusionRandomFlip3D, GridMask, ImageAug3D)
 from .transfusion_head import ConvFuser, TransFusionHead
+from .depthcustom import DepthCustom
 from .utils import (BBoxBEVL1Cost, HeuristicAssigner3D, HungarianAssigner3D,
                     IoU3DCost)
 
@@ -16,5 +17,5 @@ __all__ = [
     'HeuristicAssigner3D', 'DepthLSSTransform', 'LSSTransform',
     'BEVLoadMultiViewImageFromFiles', 'BEVFusionSparseEncoder',
     'TransformerDecoderLayer', 'BEVFusionRandomFlip3D',
-    'BEVFusionGlobalRotScaleTrans'
+    'BEVFusionGlobalRotScaleTrans','DepthCustom'
 ]
